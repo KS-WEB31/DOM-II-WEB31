fe25d1e2b0c9eb8fb73455be24b7bb2a7d2b08cb
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-05 16:07:52 -0400

    added two events and prevented default on the nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // Your code goes here
+
+//Event 1 - click
+
+//Event 2 - keydown
+
+//Event 3 - mousemove
+
+//Event 4 - scroll
+
+//Event 5 - dblclick
+
+//Event 6 - resize
+
+//Event 7 - mouseover
+const buttons = document.querySelectorAll('.btn');
+function enlarge(event){
+    event.target.style.transform = 'scale(1.1)';
+    event.target.style.transition = 'transform 1s';
+}
+buttons.forEach((btnEl) => {
+    btnEl.addEventListener('mouseenter', enlarge);
+})
+
+//Event 8 - mouseout
+function shrink(event){
+    event.target.style.transform = 'scale(1)';
+}
+buttons.forEach((btnEl) => {
+    btnEl.addEventListener('mouseout', shrink);
+})
+
+//Event 9 - 
+
+//Event 10 - 
+
+//Stop default behaviour on nav links
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach((navLinkEl) => {
+    navLinkEl.addEventListener('click', (event) => {
+        event.preventDefault();
+    })
+})
